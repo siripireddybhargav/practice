3b231b7b9a3dbe4378bd66239ac3e98a71cbad44
siripireddybhargav <bhargavsiripireddy307@gmail.com>
2022-06-05 23:33:56 +0530

    test commit 1

--- Practice/src/com/practice/Practice.java
+++ Practice/src/com/practice/Practice.java
@@ -3,6 +3,6 @@ package com.practice;
 public class Practice {
 
 	public static void main(String[] args) {
-		System.out.println("Test class Demo");
+		System.out.println("Test class Demo 1");
 	}
 }
